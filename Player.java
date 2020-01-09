55a4e5f2574b6a47d6f9e889d7e31ced6fab28ca
lucwillemse@live.nl <lucwillemse@live.nl>
2020-01-09 12:16:55 +0100

    next step in Player.java

--- Player.java
+++ Player.java
@@ -5,5 +5,23 @@ public class Player
     private String name;
     private Room currentRoom;
     private HashMap<String, Item> itemsCarring;
+
+    // constructor 
+    public Player(String name) {
+        this.name = name;
+        itemsCarring = new HashMap<String, Item>();
+    }
+    // in what room the player is
+    public Room getCurrentPlayerRoom() {
+		return currentRoom;
+    }
+    // get player name
+    public String getPlayerName() {
+		return name;
+    }
+    // set player name
+    public void setPlayerName(String name) {
+		this.name = name;
+    }
     
 }
