66ee5fff2a42f6f51e81cb16ab43eb0994437405
rhm.hendriks@outlook.com <r.h.m.hendriks@st.hanze.nl>
2020-01-14 15:02:23 +0100

    Work on question

---
+++ Question.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+/**
+ * Class Qeustion - The questions of the games
+ *
+ * This class is part of the "World of Zuul" application. 
+ * "World of Zuul" is a very simple, text based adventure game.  
+ *
+ * A "Player" represents the person playing the game. A player object stores 
+ * some important user specific settings like the name of the user and the 
+ * diffeculty of the game. 
+ * 
+ * The player object also stores some realtime game information like the 
+ * time that the user is playing, how many lives the player has left and the 
+ * amount of batlehealth that the player has left. 
+ * 
+ * @author  Ronald H.M. Hendriks, Nivard Ypey and Luc Willemse
+ * @version 2019.01.08
+ */
+
+public class Question
+{
+    // Initialization of the needed variables
+        private String question;
+        private ArrayList<String> answers;
+        private int diffeculty;
+        private int category;
+
+    public Question(String theQuestion, String theAnswers, int theDiffeculty, int theCategory){
+        question = theQuestion;
+        answers.add(e)
+        
+        List<String> items = Arrays.asList(str.split("\\s*,\\s*"));
+
+    }
+
+
+
+}
